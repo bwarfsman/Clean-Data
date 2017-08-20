a32d1556e788211e22262aa6b1f91834f610d981
bwarfsman <bwarfsman@gmail.com>
2017-08-20 13:36:42 -0400

    initial

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Coursera Getting and Cleaing Data
+# Final Project
+
+Project to download raw smartphone data and create a R script to clean it 
+and create a new table
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+Will need R installed 
+with dply package
+
+## Built With
+
+* [Rstudio](http://www.rstudio.com)
+
+## Repository inclues the script and a codebook
+
+* **Bob Warfsman
+[PurpleBooth](https://github.com/PurpleBooth)
+
---
+++ run_Analysis.R
@@ -0,0 +1,70 @@
+## Data is from http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+## which holds data from a test done using Samsung Galaxy smartphones.  The test
+## gathered data on the phone during different activities.
+##
+## This script first downloads the data.  It then loads the data and stores it in two
+## variables, test and train.  It then combines both datasets into one and deletes 
+## the original test and train.  It them removes all variables except the ones that repesent
+## standard deviations and means.  It then creates a new dataset with only the average measurement
+## for each variable by activity and subject that participated.
+
+library(dplyr)
+##Down load dataset into working directory
+fileUrl<-'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+download.file(fileUrl,dest='./Data.zip',method='curl')
+##unzip the dataset into working directory
+unzip('./Data.zip',exdir='./')
+
+## change working directory to unzipped folder
+setwd('UCI HAR Dataset/')
+
+##Load train dataset
+trData<-read.table('train/X_train.txt')
+trLabel<-read.table('features.txt')
+trLabel<-as.character(trLabel$V2)
+names(trData)<-trLabel
+trData<-cbind(read.table('train/y_train.txt'),trData)
+trData<-cbind(read.table('train/subject_train.txt'),trData)
+
+##Load test dataset
+teData<-read.table('test/X_test.txt')
+teLabel<-read.table('features.txt')
+teLabel<-as.character(teLabel$V2)
+names(teData)<-teLabel
+teData<-cbind(read.table('test/y_test.txt'),teData)
+teData<-cbind(read.table('test/subject_test.txt'),teData)
+
+
+
+##combine test and train
+setData<-rbind(teData,trData)
+remove(teData)
+remove(trData)
+
+## Set the column names for activity and subject
+colnames(setData)[1]<-'Subject'
+colnames(setData)[2]<-'Activity'
+
+## load the activity labels
+activities<-read.table('activity_labels.txt')
+activities<-activities$V2
+## change values in activity column to activity names
+setData$Activity<-factor(setData$Activity,labels=activities)
+
+##extract only the standard deviation and mean measurments
+##get list of columns to keep
+##create a character vector and add 1 and 2 that represent the 
+## forst two columns that need to be kept
+keep<-as.integer(c(1,2))
+
+## use regex to find all columns that are "std()" or "mean()"
+keep<-append(keep,grep('mean\\(|std\\()',names(setData)))
+
+## use the resulting to vector to remove all unwanted columns
+setData<-setData[,keep]
+
+## group the dataset by subject and activity and create a new data set that has only the mean of each
+## measured variable by subject and activity
+setData<-group_by(setData,Subject,Activity)
+newSetData<-summarise_all(setData,funs(mean))
+
